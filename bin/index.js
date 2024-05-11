a2567d82839f14733011402b5619b5a75aaedd78
hot <masterss12@yandex.ru>
2024-05-11 18:58:51 +0300

    update

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,6 @@ const { create } = require("create-strapi-types");
 let hasFrontend = false;
 
 process.argv.forEach(function (val, index, array) {
-  console.log(index + ": " + val);
   if (val === "frontend") {
     hasFrontend = true;
   }
